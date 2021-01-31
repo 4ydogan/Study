06dba17cbd87c0103eeedbc0701066aab6b34f8c
4ydogan <68594739+4ydogan@users.noreply.github.com>
2021-01-31 17:59:35 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,12 +1,7 @@
-
 public class Fibonacci {
 	public static int fib(int index) {
 		if(index == 1 || index == 2)
 			return 1;
 		return fib(index-1) + fib(index-2);
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(fib(10));
 	}
 }
